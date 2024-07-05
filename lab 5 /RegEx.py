cfcc1bef53da708e67aef9ebc508695c69d3dd23
RinAriRin <rinatova04@inbox.ru>
2024-07-05 13:13:05 +0600

    Update RegEx.py

--- lab 5 /RegEx.py
+++ lab 5 /RegEx.py
@@ -1,44 +1,58 @@
 #task 1 Напишите программу на Python, которая соответствует строке, за которой следует ноль или более 's.'a''b'
 import re
-txt = input()
-x = re.search('a.*b', txt)
-print(x)
+txt = input("Введите строку: ")
+x = re.search(r'a(s*)b', txt)
+if x:
+    print("Найдено совпадение:", x.group())
+else:
+    print("Совпадений не найдено")
 
 #task 2 Напишите программу на Python, которая соответствует строке, за которой следует от двух до трех .'a''b'
-txt = input()
-x = re.search('a.{1}b', txt)
-y = re.search('a.{2}b', txt)
-if re.search('a.{1}b', txt):
-    print(x)
-elif re.search('a.{2}b', txt):
-    print(y)
+import re
+txt = input("Введите строку: ")
+x = re.search(r'a(b{2,3})', txt)
+if x:
+    print("Найдено совпадение:", x.group())
 else:
-    print("Error")
+    print("Совпадений не найдено")
 
 #task 3 Напишите программу на Python для поиска последовательностей строчных букв, соединенных подчеркиванием.
-txt = 'wgeerger_egregerignerio'
-x = re.findall("[a-z]+\_+[a-z]", txt)
+import re
+txt = input("Введите строку: ")
+x = re.findall(r"[a-z]+_[a-z]+", txt)
 if x:
-    print(x)
+    print("Найдено совпадение:", x)
 else:
-    print("No match")
+    print("Совпадений не найдено")
+
 
 #task 4  Напишите программу на Python для поиска последовательностей одной прописной буквы, за которой следуют строчные буквы.
-txt = 'WFIWFNWFRoerjoiernwif'
-x = re.findall('[A-Z]{1}[a-z]+', txt)
-print(x)
+import re
+
+txt = input("Введите строку: ")
+x = re.findall(r'[A-Z][a-z]+', txt)
+if x:
+    print("Найдено совпадение:", x)
+else:
+    print("Совпадений не найдено")
 
 #task 5 Напишите программу на Python, которая соответствует строке, за которой следует что-либо, оканчивающееся на .'a''b'
-txt = 'wkgmwrgiuageoigjoijwfb'
-x = re.search('a.*b$', txt)
-print(x)
+import re
+txt = input("Введите строку: ")
+x = re.search(r'a.*b$', txt)
+if x:
+    print("Найдено совпадение:", x.group())
+else:
+    print("Совпадений не найдено")
 
 #task 6 Напишите программу на Python, чтобы заменить все вхождения пробела, запятой или точки двоеточием.
-txt = 'nernnero imerimer,ofnrmer.wpomermpr'
-x = re.sub('[ .,]', ';', txt, 4)
+import re
+txt = input("Введите строку: ")
+x = re.sub(r'[ .,]', ':', txt)
 print(x)
 
 #task 7 Напишите программу на python для преобразования строки регистра змеи в строку регистра верблюда.
+import re
 txt = 'hello_world'
 txt2 = txt.upper()
 camel = ''
@@ -54,21 +68,24 @@ camel = re.sub('_', '', camel)
 print(camel)
 
 #task 8 Напишите программу на Python для разделения строки на прописные буквы.
-txt = 'HelloWorld'
+import re
+txt = input("Введите строку: ")
 x = re.findall('[A-Z][^A-Z]*', txt)
 print(x)
 
 #task 9 Напишите программу на Python для вставки пробелов между словами, начинающимися с заглавных букв.
-
+import re
 str = "HelloWorld"
 x = re.sub(r"([A-Z])", r" \1", str)
 print(x)
 
 #task 10 Напишите программу на Python для преобразования заданной строки верблюжьего регистра в змеиный регистр.
+import re
 
-camel = 'HelloWorld'
-snake = re.sub(r"([A-Z])", r" \1", camel)
+camel = input("Введите строку в верблюжьем регистре: ")
+snake = re.sub(r"([A-Z])", r"_\1", camel)
 snake = snake.lower()
-snake = snake.strip()
-snake = re.sub(r'\s', '_', snake)
-print(snake)
+if snake.startswith('_'):
+    snake = snake[1:]
+print("Строка в змеином регистре:", snake)
+
